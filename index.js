9e27f47e0a62d684b70e81f686425a8e9bb2e7c5
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2019-10-06 16:32:26 +0200

    up index.js to add mysql connection

--- index.js
+++ index.js
@@ -2,13 +2,31 @@
 * @Author: Baptiste Bertrand-Rapello
 * @Date:   2019-10-03 11:34:19
 * @Last Modified by:   Baptiste Bertrand-Rapello
-* @Last Modified time: 2019-10-03 12:26:11
+* @Last Modified time: 2019-10-06 16:29:33
 */
 
 const express = require('express')
 const app = express();
 
 const glb_port = 5000
+const glb_host_mysql = "192.168.1.22"
+const glb_usr_mysql = "root"
+const glb_pw_mysql = "Bonjour2394"
+
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: glb_host_mysql,
+  user: glb_usr_mysql,
+  password: glb_pw_mysql
+});
+
+con.connect(function(err) {
+	console.log("ici je vais me connecter");
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 app.get('/', (req, res) => {
 	var theDate = new Date()
