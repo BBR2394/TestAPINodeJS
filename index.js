23ae3b04f4ecd1af245fdcd902b9481cdc697739
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2019-10-06 21:45:53 +0200

    test SELECT in database

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 * @Author: Baptiste Bertrand-Rapello
 * @Date:   2019-10-03 11:34:19
 * @Last Modified by:   Baptiste Bertrand-Rapello
-* @Last Modified time: 2019-10-06 16:29:33
+* @Last Modified time: 2019-10-06 21:45:36
 */
 
 const express = require('express')
@@ -12,6 +12,7 @@ const glb_port = 5000
 const glb_host_mysql = "localhost"
 const glb_usr_mysql = "root"
 const glb_pw_mysql = "Bonjour2394"
+const glb_db = "Chevaux_test1"
 
 
 var mysql = require('mysql');
@@ -19,7 +20,8 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: glb_host_mysql,
   user: glb_usr_mysql,
-  password: glb_pw_mysql
+  password: glb_pw_mysql,
+  database: glb_db
 });
 
 con.connect(function(err) {
@@ -30,6 +32,10 @@ con.connect(function(err) {
 	throw err;
     }
     console.log("Connected!");
+    con.query("SELECT * FROM Chevaux", function (err, result, fields) {
+    if (err) throw err;
+    	console.log(result);
+  	});
 });
 
 app.get('/', (req, res) => {
